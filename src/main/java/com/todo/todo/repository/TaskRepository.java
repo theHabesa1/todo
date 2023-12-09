1014feddb97af76687f44936c9b0039da173d854
Yared Abera <90505588+theHabesa1@users.noreply.github.com>
2023-12-09 17:54:39 +0300

    Update TaskRepository.java

--- src/main/java/com/todo/todo/repository/TaskRepository.java
+++ src/main/java/com/todo/todo/repository/TaskRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 
 public interface TaskRepository extends MongoRepository<Task, String> {
-    // You can add custom queries here if needed
+   
 }
